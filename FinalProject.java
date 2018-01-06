0d98fe9c7e6f7dd4b668bdba2f3b92e7ce425bbb
Vince <vlin6@stuy.edu>
2018-01-06 03:33:09 -0500

    failed tests

--- FinalProject.java
+++ FinalProject.java
@@ -1,3 +1,40 @@
-public class FinalProject{
-//vincent's Branch
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FinalProject extends JFrame implements KeyListener{
+    private Container pane;
+    private Player player = new Player();
+
+    public FinalProject(){
+	this.setTitle("Our Game");
+	this.setSize(600,800);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+
+	
+
+	/*pane.add(player);*/
+    }
+
+    public void keyPressed(KeyEvent e){
+	char s = e.getKeyChar();
+	System.out.println(s + "");
+	
+    }
+
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    public void keyTyped(KeyEvent e){
+	char s = e.getKeyChar();
+	System.out.println(s + "");
+    }
+
+    public static void main(String[]args){
+	FinalProject g = new FinalProject();
+	g.setVisible(true);
+    }
 }
