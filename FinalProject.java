802d56657f8d1fd5b519ce65b29de3d7d9efb0a3
Vince <vlin6@stuy.edu>
2018-01-06 03:05:07 -0500

    added imports

--- FinalProject.java
+++ FinalProject.java
@@ -1,3 +1,9 @@
-public class FinalProject{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class FinalProject extends Jframe implements keyListener{
+    private container pane;
+    private Rectangle player, hazard;
+    
 }
