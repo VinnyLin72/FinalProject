22a7edf0136010b7c3ef0e8206da44bed9d5be64
“Vincent <vlin6@stuy.edu>
2018-01-08 22:51:09 -0500

    messed around with player class

--- Player.java
+++ Player.java
@@ -2,33 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Player extends Rectangle{
-
+public class Player extends Rectangle implements KeyListener{
     int x = 300;
     int y = 300;
     int xa = 1;
     int ya = 1;
+    
+    
     private int lives;
     private boolean alive = true;
     private FinalProject game;
-    
+
     public Player(FinalProject game){
 	this.game = game;
 	lives = 3;
     }
 
-    public void move(){
-	if (x + xa < 0)
-	    xa = 1;
-	if (x + xa > game.getWidth() - 30)
-	    xa = -1;
-	if (y + ya < 0)
-	    ya = 1;
-	if (y + ya > game.getHeight() - 30)
-	    ya = -1;
+    public void keyPressed(KeyEvent e) {
 
-	x = x + xa;
-	y = y + ya;
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    public void move(){
+
     }
 
     public void hurt(){
