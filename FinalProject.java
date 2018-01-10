b8b365f5562c2679889c9bf0146f8cb6d947bbff
VinnyLin72 <33820766+VinnyLin72@users.noreply.github.com>
2018-01-09 22:59:48 -0500

    Add files via upload

---
+++ FinalProject.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FinalProject extends JPanel{
+    public Player player = new Player(this);
+    
+   
+    private void move(){
+	player.move();
+    }
+    public void paint(Graphics g){
+	super.paint(g);
+	player.paint(g);
+    }
+
+    public static void main (String[]args)throws InterruptedException{
+	JFrame frame = new JFrame("game");
+	FinalProject game = new FinalProject();
+	frame.add(game);
+	frame.setSize(500,500);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	while (true){
+	    game.move();
+	    game.repaint();
+	    Thread.sleep(10);
+	}
+    }
+}
